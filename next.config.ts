d99045d763de4cae18f244384f527d4f95f0958e
Daiyan Ar Rahman (Minecraft Boy95 D) <daiyand691@gmail.com>
2025-08-25 14:48:52 +0000

    Yeah problem solved but Why I cant see any yt vids or any vedio sugessti

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.ytimg.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
   experimental: {
